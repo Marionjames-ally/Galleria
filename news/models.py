82d0db75a61bc1d231b0a9f6267b2ca0d830a43d
Marionjames-ally <marionjames2018@gmail.com>
2020-02-29 12:25:58 +0300

    fix bugs in the app model and the app url

--- news/models.py
+++ news/models.py
@@ -3,21 +3,18 @@ import datetime as dt
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField(upload_to='gallery/'),
-    image_title =models.CharField(max_length =30),
-    image_description = models.CharField(max_length = 150),
+    post_image = models.ImageField(upload_to = 'images/', default = 'image')
+    image_title = models.CharField(max_length = 100, default = 'title')
+    image_description = models.TextField(max_length=200, default = 'description')
     image_location = models.ForeignKey("Location", on_delete=models.CASCADE)
     image_category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    image_post_date = models.DateTimeField(auto_now_add=True)
+    image_post_date =models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.image_title
 
     def save_image(self):
         self.save
-
-    # class Meta:
-    #     ordering = ['title']
 
     @classmethod
     def get_images(cls):
@@ -37,7 +34,7 @@ class Location(models.Model):
         cls.objects.filter(id = id).delete()
 
 class Category(models.Model):
-    category = models.CharField(max_length = 30),
+    category = models.CharField(max_length = 60, default = 'category')
 
 
 
